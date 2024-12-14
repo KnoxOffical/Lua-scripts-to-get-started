af68b4cffe8ef3449ef4fc2211347a1ad01e7071
astrid <157550461+KnoxOffical@users.noreply.github.com>
2024-12-13 22:17:48 -0500

    variable-creation.lua

--- code/variable-creation.lua
+++ code/variable-creation.lua
@@ -1,3 +1,5 @@
-//you do not use "var" as in javascript or java instead you just put it like this:
+--you do not use "var" as in javascript or java instead you just put it like this:
+local j --Lua uses local as it's var
+local x
 j = 8
 x = 10
