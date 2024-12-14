9a77adf04d889026281d2c452dbc731f535456bb
astrid <157550461+KnoxOffical@users.noreply.github.com>
2024-12-14 17:41:42 -0500

    date.lua

--- code/date.lua
+++ code/date.lua
@@ -4,3 +4,15 @@ day = os.date("%d")
 hour = os.date("%H")
 minute = os.date("%M")
 second = os.date("%S")
+
+
+
+-- only found this in OneComplier due to my usage on replit being used up:
+year = os.date("%Y")
+day = os.date("%d")
+month = os.date("%m")
+fulldate = month .."/" ..day .."/" ..year
+print(fulldate)
+while tonumber(day) == 14 do -- this makes it able to infinitely get the number and display it in the console.
+  print(day)
+end
